942256a29948cf15282edaa7a998bd51b8cecbbb
Laura <Laura@DESKTOP-BESSB6E>
2023-08-24 23:59:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Iveskite skaiciu:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 3 == 0 && number % 5 == 0)
+{
+    Console.WriteLine("BazingaPop");
+}
+else if (number % 3 == 0)
+{
+    Console.WriteLine("Bazinga");    
+}
+else if (number % 5 == 0)
+{
+    Console.WriteLine("Pop");
+}
+else 
+{
+    Console.WriteLine(number);
+}
